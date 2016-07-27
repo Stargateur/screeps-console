fb54d97ee37d90e7c3123f5705c797657bfe42c4
plasko_a <antoine.plaskowski@epitech.eu>
2016-07-27 09:02:03 +0200

    init

---
+++ console.js
@@ -0,0 +1,45 @@
+"use strict";
+const sockjs = require("sockjs-client");
+const request = require("request");
+let sock = new sockjs("https://screeps.com/socket");
+let config = require("./console.json");
+sock.onopen = function () {
+    let auth = {
+        url: "https://screeps.com/api/auth/signin",
+        json: true,
+        body: {
+            email: config.user,
+            password: config.password
+        }
+    };
+    request.post(auth, function (err, httpResponse, body) {
+        let token = body.token;
+        let info = {
+            url: "https://screeps.com/api/auth/me",
+            json: true,
+            headers: {
+                "X-Token": token,
+                "X-Username": config.user,
+            },
+        };
+        request.get(info, function (err, httpResponse, body) {
+            sock.send("auth " + token);
+            sock.send("subscribe user:" + body._id + "/console");
+        });
+    });
+};
+sock.onmessage = function (message) {
+    try {
+        let json = JSON.parse(message.data);
+        if (json[0] && json[1] && json[1].messages && json[1].messages.log) {
+            let log = json[1].messages.log;
+            for (let i in log) {
+                console.log(new Date(message.timeStamp), log[i]);
+            }
+        }
+    }
+    catch (e) {
+    }
+};
+sock.onclose = function () {
+};
